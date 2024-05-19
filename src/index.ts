2e287b90f29b0e3888f9dd4e0d2ae2e49eb68ebe
mochatek <akashsabu@ymail.com>
2024-05-19 08:47:30 +0530

    fix: Add encoding param and read as string

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,56 @@
 import { createReadStream } from "fs";
 import { once } from "events";
 
+type BufferEncoding =
+  | "ascii"
+  | "utf8"
+  | "utf-8"
+  | "utf16le"
+  | "utf-16le"
+  | "ucs2"
+  | "ucs-2"
+  | "base64"
+  | "base64url"
+  | "latin1"
+  | "binary"
+  | "hex";
+
 type ReadStreamOptions = {
-  signal?: AbortSignal;
+  encoding?: BufferEncoding;
   highWaterMark?: number;
+  signal?: AbortSignal;
 };
 
-const BRACKET = {
-  OPEN: "[",
-  CLOSED: "]",
+const CHARACTER = {
+  OPEN_BRACKET: `[`,
+  CLOSE_BRACKET: `]`,
 };
 
-async function createDataStream(path: string, options?: ReadStreamOptions) {
+async function createReadableFileStream(path: string, options?: ReadStreamOptions) {
+  if (!options) {
+    options = {}
+  }
+  if (!options.encoding) {
+    options.encoding = 'utf-8'
+  }
+
   const stream = createReadStream(path, options);
   await once(stream, "readable");
 
-  const getChunk = async function* () {
+  const chunkGenerator = async function* () {
     for await (const chunk of stream) {
       yield chunk;
     }
   };
 
-  return { stream, getChunk };
+  return { stream, chunkGenerator };
 }
 
-async function createJsonStream<T extends any[]>(
+async function createJsonChunkStream<T extends any[]>(
   path: string,
   options?: ReadStreamOptions
 ) {
-  const { stream, getChunk } = await createDataStream(path, options);
+  const { stream, chunkGenerator } = await createReadableFileStream(path, options);
 
   return async function* (chunkSize: number) {
     let rootDetected = false;
@@ -36,11 +58,9 @@ async function createJsonStream<T extends any[]>(
     let chunkBuffer = "";
     let resultBuffer: T[] = [];
 
-    for await (const chunk of getChunk()) {
-      for (let i = 0; i < chunk.length; i++) {
-        const char = String.fromCharCode(chunk[i]);
-
-        if (char === BRACKET.OPEN) {
+    for await (const chunk of chunkGenerator()) {
+      for (let char of chunk) {
+        if (char === CHARACTER.OPEN_BRACKET) {
           if (!rootDetected) {
             rootDetected = true;
             continue;
@@ -48,7 +68,7 @@ async function createJsonStream<T extends any[]>(
             chunkBuffer = `${chunkBuffer}${char}`;
             bracketCount += 1;
           }
-        } else if (char === BRACKET.CLOSED) {
+        } else if (char === CHARACTER.CLOSE_BRACKET) {
           chunkBuffer = `${chunkBuffer}${char}`;
           bracketCount -= 1;
 
@@ -81,4 +101,4 @@ async function createJsonStream<T extends any[]>(
   };
 }
 
-export { createJsonStream as createReadStream };
+export { createJsonChunkStream as createReadStream };
