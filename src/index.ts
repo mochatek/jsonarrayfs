2d8d1f4d3be7d2dac480fd334269224974d9b924
mochatek <akashsabu@ymail.com>
2024-05-25 14:11:31 +0530

    refactor: Implement the logic in class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { createReadStream } from "fs";
+import { ReadStream, createReadStream } from "fs";
 import { once } from "events";
 
 type BufferEncoding =
@@ -21,7 +21,7 @@ type ReadStreamOptions = {
   signal?: AbortSignal;
 };
 
-type ElementType = "array" | "object";
+type ElementType = "array" | "object" | "string" | "others";
 
 const CHARACTER = {
   BRACKET: {
@@ -34,95 +34,173 @@ const CHARACTER = {
   },
   QUOTE: `"`,
   ESCAPE: `\\`,
+  SPACE: " ",
+  COMMA: ",",
+  NEW_LINE: "\n",
 };
 
-async function createReadableFileStream(
-  path: string,
-  options?: ReadStreamOptions
-) {
-  if (!options) {
-    options = {};
-  }
-  if (!options.encoding) {
-    options.encoding = "utf-8";
+class JsonArrayStreamer<T> {
+  private readStream: ReadStream | null;
+  private rootDetected: boolean;
+  private elementDetected: boolean;
+  private elementType: ElementType;
+  private elementParser: ((char: string) => void) | null;
+  private elementEnclosureCount: number;
+  private isCharInsideQuotes: boolean;
+  private isCharEscaped: boolean;
+  private chunkBuffer: string;
+  private resultBuffer: T[];
+
+  private constructor() {
+    this.readStream = null;
+    this.rootDetected = false;
+    this.elementDetected = false;
+    this.elementType = "others";
+    this.elementParser = null;
+    this.elementEnclosureCount = 0;
+    this.isCharInsideQuotes = false;
+    this.isCharEscaped = false;
+    this.chunkBuffer = "";
+    this.resultBuffer = [];
   }
 
-  const stream = createReadStream(path, options);
-  await once(stream, "readable");
+  private async *chunkGenerator() {
+    if (!this.readStream) throw new Error("Stream not initialized");
 
-  const chunkGenerator = async function* () {
-    for await (const chunk of stream) {
+    for await (const chunk of this.readStream) {
       yield chunk;
     }
-  };
+  }
 
-  return { stream, chunkGenerator };
-}
+  private resetParser() {
+    this.elementDetected = false;
+    this.elementType = "others";
+    this.elementParser = null;
+    this.elementEnclosureCount = 0;
+    this.isCharInsideQuotes = false;
+    this.isCharEscaped = false;
+    this.chunkBuffer = "";
+  }
 
-async function createJsonChunkStream<T extends any[] | Record<any, any>>(
-  path: string,
-  elementType: ElementType,
-  options?: ReadStreamOptions
-) {
-  const { stream, chunkGenerator } = await createReadableFileStream(
-    path,
-    options
-  );
-  const ELEMENT_WRAPPER =
-    elementType === "object" ? CHARACTER.BRACE : CHARACTER.BRACKET;
+  private primitiveElementParser(char: string) {
+    
+  }
 
-  return async function* (chunkSize: number) {
-    let rootDetected = false;
-    let elementWrapperCount = 0;
-    let insideQuotes = false;
-    let isEscaped = false;
-    let chunkBuffer = "";
-    let resultBuffer: T[] = [];
+  private containerElementParser(char: string) {
+    const ENCLOSURE = this.elementType === 'array'? CHARACTER.BRACKET: CHARACTER.BRACE;
 
-    readChunks: for await (const chunk of chunkGenerator()) {
+    if (char === ENCLOSURE.OPEN && !this.isCharInsideQuotes) {
+      this.chunkBuffer = `${this.chunkBuffer}${char}`;
+      this.elementEnclosureCount += 1;
+    } else if (char === ENCLOSURE.CLOSE && !this.isCharInsideQuotes) {
+      this.chunkBuffer = `${this.chunkBuffer}${char}`;
+      this.elementEnclosureCount -= 1;
+
+      if (this.elementEnclosureCount === 0) {
+        const element = JSON.parse(this.chunkBuffer);
+        this.resultBuffer.push(element);
+        this.resetParser();
+      }
+    } else if (this.chunkBuffer.length) {
+      this.chunkBuffer = `${this.chunkBuffer}${char}`;
+
+      if (char === CHARACTER.ESCAPE) {
+        this.isCharEscaped = !this.isCharEscaped;
+      } else if (char === CHARACTER.QUOTE && !this.isCharEscaped) {
+        this.isCharInsideQuotes = !this.isCharInsideQuotes;
+      } else if (this.isCharEscaped) {
+        this.isCharEscaped = false;
+      }
+    }
+  }
+
+  public async *stream<T>(chunkSize: number) {
+    for await (const chunk of this.chunkGenerator()) {
       for (let char of chunk) {
-        if (char === CHARACTER.BRACKET.OPEN && !rootDetected) {
-          rootDetected = true;
+        if (!this.rootDetected) {
+          this.rootDetected = char === CHARACTER.BRACKET.OPEN;
           continue;
-        } else if (char === CHARACTER.BRACKET.CLOSE && !elementWrapperCount) {
-          if (!resultBuffer.length) break readChunks;
+        }
 
-          yield resultBuffer.splice(0, chunkSize);
-        } else if (char === ELEMENT_WRAPPER.OPEN && !insideQuotes) {
-          chunkBuffer = `${chunkBuffer}${char}`;
-          elementWrapperCount += 1;
-        } else if (char === ELEMENT_WRAPPER.CLOSE && !insideQuotes) {
-          chunkBuffer = `${chunkBuffer}${char}`;
-          elementWrapperCount -= 1;
+        if (!this.elementDetected) {
+          this.elementDetected = ![
+            CHARACTER.SPACE,
+            CHARACTER.COMMA,
+            CHARACTER.NEW_LINE,
+            CHARACTER.BRACKET.CLOSE,
+          ].includes(char);
+        }
 
-          if (elementWrapperCount === 0) {
-            const element: T = JSON.parse(chunkBuffer);
-            resultBuffer.push(element);
-            chunkBuffer = "";
-
-            if (resultBuffer.length === chunkSize) {
-              if (!stream.closed) stream.pause();
-              yield resultBuffer.splice(0, chunkSize);
-              if (!stream.closed) stream.resume();
+        if (this.elementDetected) {
+          if (!this.elementParser) {
+            if (char === CHARACTER.BRACKET.OPEN) {
+              this.elementType = "array";
+              this.elementParser = this.containerElementParser;
+            } else if (char === CHARACTER.BRACE.OPEN) {
+              this.elementType = "object";
+              this.elementParser = this.containerElementParser;
+            } else if (char === CHARACTER.QUOTE) {
+              this.elementType = "string";
+              this.elementParser = this.primitiveElementParser;
+            } else {
+              this.elementType = "others";
+              this.elementParser = this.primitiveElementParser;
             }
           }
-        } else if (chunkBuffer.length) {
-          chunkBuffer = `${chunkBuffer}${char}`;
 
-          if (char === CHARACTER.ESCAPE) {
-            isEscaped = !isEscaped;
-          } else if (char === CHARACTER.QUOTE && !isEscaped) {
-            insideQuotes = !insideQuotes;
-          } else if (isEscaped) {
-            isEscaped = false;
+          this.elementParser(char);
+
+          if (this.resultBuffer.length === chunkSize) {
+            if (!this.readStream?.closed) this.readStream?.pause();
+            yield this.resultBuffer.splice(0, chunkSize);
+            if (!this.readStream?.closed) this.readStream?.resume();
           }
         }
       }
     }
 
-    await stream.close();
-    return resultBuffer;
+    this.readStream?.close();
+    this.readStream = null;
+
+    if (this.chunkBuffer.length) {
+      const element = JSON.parse(this.chunkBuffer)
+      this.resultBuffer.push(element)
+      this.resetParser()
+    }
+    if (this.resultBuffer.length) {
+      yield this.resultBuffer.splice(0);
+    }
+
+    return this.resultBuffer;
+  }
+
+  private static getReadableFileStream = (
+    path: string,
+    options?: ReadStreamOptions
+  ) => {
+    if (!options) {
+      options = {};
+    }
+    if (!options.encoding) {
+      options.encoding = "utf-8";
+    }
+
+    const readStream = createReadStream(path, options);
+    return readStream;
+  };
+
+  public static create = async <T>(
+    path: string,
+    options?: ReadStreamOptions
+  ) => {
+    const instance = new JsonArrayStreamer<T>();
+    instance.readStream = JsonArrayStreamer.getReadableFileStream(
+      path,
+      options
+    );
+    await once(instance.readStream, "readable");
+    return instance;
   };
 }
 
-export { createJsonChunkStream as createReadStream };
+export { JsonArrayStreamer };
